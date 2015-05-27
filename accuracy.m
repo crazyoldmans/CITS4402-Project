7fb479c167e0507c9a761958236b0284e6021078
Dom Cockman <20927611@ECM-W-C-205-024.uniwa.uwa.edu.au>
2015-05-27 21:42:50 +0800

    fixed accuracy.m

--- accuracy.m
+++ accuracy.m
@@ -1,17 +1,23 @@
+%finds the accuracy of the algorithm as a percentage of correct objects 
+%found over the number of objects in the scene
+
 function acc = accuracy( detected_obj, scn)
 
     hyph = strsplit(scn, '-');
-    pgm = strsplit(hyph{2}, '.');
-    tmp = strsplit(pgm{1},'_');
-    count = 0;
-    for i = 1:length(detected_obj)
-        for j = 1:length(tmp)
-            if strcmp(tmp{j},detected_obj{i})
-                count  = count+1;
+    if length(hyph) >1
+        pgm = strsplit(hyph{2}, '.');
+        tmp = strsplit(pgm{1},'_');
+        count = 0;
+        for i = 1:length(detected_obj)
+            for j = 1:length(tmp)
+                if strcmp(tmp{j},detected_obj{i})
+                    count  = count+1;
+                end
             end
         end
+        acc = 100 * count/length(tmp);
+    else
+        acc = 0;
     end
-      
-    acc = 100 * count/length(tmp);
 
 end
