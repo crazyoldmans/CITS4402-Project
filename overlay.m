186cca604f370a4d235e8c2b8c7277db65e35ecd
Dom Cockman <20927611@ECM-W-C-205-024.uniwa.uwa.edu.au>
2015-05-27 21:46:18 +0800

    commented overlay

--- overlay.m
+++ overlay.m
@@ -4,13 +4,15 @@
 %   displays lines connecting the matched keypoints.  A match is accepted
 %   only if its distance is less than distRatio times the distance to the
 %   second closest match.
+%   It also outlines the object, localizing it within the scene
 % It returns the number of matches displayed.
 %
 % Example: match('scene.pgm','book.pgm');
 %
 % MODIFIED FOR CITS4402 PROJECT
 function overlay(im1, im2, des1, loc1, loc2, match, data)
-    % Create a new image showing the two images side by side.
+    % Calculate the affine transformations necessary to move the outline
+    %of the object of concern over on to the scene image
 
     x = 0;
     f = 0;
@@ -34,7 +36,10 @@ function overlay(im1, im2, des1, loc1, loc2, match, data)
             break
         end
     end
-    if x ~= 0 && f~=0 && z ~= size(des1,1) && x ~= size(des1,1) && f ~= size(des1,1) && f ~=x+1
+    
+    %if there are sufficient key point matches, begin calculating the 
+    %required transformations for the outline
+    if x ~= 0 && f~=0 && z ~= size(des1,1) && x ~= size(des1,1) && f ~= size(des1,1)
         
         BW = im2bw(im1);
         outline = bwperim(BW);
@@ -53,21 +58,14 @@ function overlay(im1, im2, des1, loc1, loc2, match, data)
         outline = imresize(outline, [newx NaN]);
         rowshift = round((loc2(match(z),1) - loc1(z,1))) + size(im1,1) - size(outline,1)
         colshift = round(((loc2(match(z),2)+size(im1,2))-loc1(z,2))) + size(im1,2) - size(outline,2)
-        %paddingc = (size(im1,2) + size(im2,2)) - size(outline,2)
-        %paddingr = (size(im1,1) + size(im2,1)) - size(outline,1)
+
         out2 = padarray(outline,[abs(size(im1,1) - size(im2,1)) size(im2,2)], 'post');
 
         outline(:,2) = outline(:,2) + colshift;
         outline(:,1) = outline(:,1) + rowshift;
-        %T = maketform('affine', [1 0 0; 0 1 0; rowshift colshift 1]);
-        %im4 = imtransform(outline, T, 'XData',[1 size(outline,2)], 'YData',[1 size(outline,1)]);
 
+    %append the two target images
         im3 = appendimages(im1,im2);
-        %im3(out2) = 255;
-
-        % Show a figure with lines joining the accepted matches.
-        %axes(data.ha1);
-        %figure('Position', [100 100 size(im3,2) size(im3,1)]);
 
         axes(data.ha1);
 
